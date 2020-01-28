00115238a75fd48ebea312135f7cf0d668173e53
jannyjie <45221067+jannyjie@users.noreply.github.com>
2020-01-28 10:56:31 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,383 @@
+// GoTop按鈕時的事件
+$(document).ready(function() {
+            // Show or hide the sticky footer button
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 100) {
+                    $('.go-top').fadeIn(100);
+                } else {
+                    $('.go-top').fadeOut(300);
+                }
+            });
+            
+            // Animate the scroll to top
+            $('.go-top').click(function(event) {
+                event.preventDefault();
+                
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
+
+// GoTop按鈕時的事件  END
+
+//彈跳視窗
+$(document).ready(function() {
+    $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg").fadeOut(300);
+      });
+  });
+    $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg01").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click01").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg01").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok01").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg01").fadeOut(300);
+      });
+  });
+    $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg02").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click02").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg02").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok02").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg02").fadeOut(300);
+      });
+  });
+
+  //後台管理
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear01").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear01").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear01").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear01").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear01").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear02").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear02").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear02").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear02").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear02").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear03").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear03").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear03").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear03").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear03").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear04").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear04").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear04").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear04").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear04").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear05").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear05").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear05").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear05").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear05").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear06").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear06").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear06").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear06").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear06").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear07").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear07").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear07").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear07").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear07").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear08").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear08").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear08").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear08").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear08").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear09").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear09").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear09").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear09").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear09").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear10").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear10").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear10").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear10").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear10").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear11").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear11").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear11").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear11").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear11").fadeOut(300);
+      });
+  });
+  $(function(){
+      // 隱藏背景與訊息對話框
+      $("#bg-Rear12").hide();
+      
+      // 點按「Click Me!」按鈕
+      $("#click-Rear12").click(function(){
+          // 淡入顯示背景與訊息對話框
+          $("#bg-Rear12").fadeIn(300);
+      });
+      
+      // 點按「OK」按鈕
+      $("#ok-Rear12").click(function(){
+          // 淡出隱藏背景與訊息對話框
+          $("#bg-Rear12").fadeOut(300);
+      });
+  });
+
+});
+
+//下拉選單
+$(document).ready(function() {
+    $(".custom-select").each(function() {
+      var classes = $(this).attr("class"),
+          id      = $(this).attr("id"),
+          name    = $(this).attr("name");
+      var template =  '<div class="' + classes + '">';
+          template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+          template += '<div class="custom-options">';
+          $(this).find("option").each(function() {
+            template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+          });
+      template += '</div></div>';
+      
+      $(this).wrap('<div class="custom-select-wrapper"></div>');
+      $(this).hide();
+      $(this).after(template);
+    });
+    $(".custom-option:first-of-type").hover(function() {
+      $(this).parents(".custom-options").addClass("option-hover");
+    }, function() {
+      $(this).parents(".custom-options").removeClass("option-hover");
+    });
+    $(".custom-select-trigger").on("click", function() {
+      $('html').one('click',function() {
+        $(".custom-select").removeClass("opened");
+      });
+      $(this).parents(".custom-select").toggleClass("opened");
+      event.stopPropagation();
+    });
+    $(".custom-option").on("click", function() {
+      $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+      $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+      $(this).addClass("selection");
+      $(this).parents(".custom-select").removeClass("opened");
+      $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+    });
+});
+
+//上傳檔案//Upload Field
+$(document).ready(function() {
+    $("form").on("change", ".file-upload-field", function(){ 
+    $(this).parent(".file-upload-wrapper").attr("data-text", $(this).val().replace(/.*(\/|\\)/, '') );
+});
+});
+
+//收合
+$(document).ready(function() {
+    $(".open").click( function () {
+      var container = $(this).parents(".topic");
+      var answer = container.find(".answer");
+      var trigger = container.find(".faq-t");
+      
+      answer.slideToggle(200);
+      
+      if (trigger.hasClass("faq-o")) {
+        trigger.removeClass("faq-o");
+      }
+      else {
+        trigger.addClass("faq-o");
+      }
+      
+      if (container.hasClass("expanded")) {
+        container.removeClass("expanded");
+      }
+      else {
+        container.addClass("expanded");
+      }
+    });
+});
+
+
+// 上傳圖片
+$('input[type="file"]').each(function(){
+  // Refs
+  var $file = $(this),
+      $label = $file.next('label')
+  
+  // When a new file is selected
+  $file.on('change', function(event){
+    var fileName = $file.val().split( '\\' ).pop(),
+        tmppath = URL.createObjectURL(event.target.files[0]);
+    //Check successfully selection
+    if( fileName ){
+      $label
+        .addClass('file-ok')
+        .css('background-image', 'url(' + tmppath + ')');
+      $labelText.text(fileName);
+    }else{
+      $label.removeClass('file-ok');
+      $labelText.text(labelDefault);
+    }
+  });
+  
+// End loop of file input elements  
+});
+    
+
+
+
+
+// 文字字數
+function CountWords(obj, show_id) {
+  var fullStr = obj.value;
+  var charCount = fullStr.length;
+  var rExp = /[^A-Za-z0-9]/gi;
+  var spacesStr = fullStr.replace(rExp, ' ');
+  var cleanedStr = spacesStr + ' ';
+  do {
+    var old_str = cleanedStr;
+    cleanedStrcleanedStr = cleanedStr.replace(' ', ' ');
+  } while (old_str != cleanedStr);
+  var splitString = cleanedStr.split(' ');
+  document.getElementById(show_id).innerHTML = charCount;
+}
---
+++ js/text.js
@@ -0,0 +1,8 @@
+// 進度條
+$(function () {
+        $("#div1").myProgress({speed: 1000, percent: 80, width: "calc(100% - 150px)", height: "40px"});
+        $("#div2").myProgress({speed: 1000, percent: 100, width: "calc(100% - 150px)", height: "40px"});
+        $("#div3").myProgress({speed: 1000, percent: 50, width: "calc(100% - 150px)", height: "40px"});
+        $("#div4").myProgress({speed: 1000, percent: 70, width: "calc(100% - 150px)", height: "40px"});
+        $("#div5").myProgress({speed: 1000, percent: 20, width: "calc(100% - 150px)", height: "40px"});
+});
